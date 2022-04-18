576508689b4c4a7cc48214d73c82a1f0070d4573
nitinjagtaqa <nitinjagtapqa@gmail.com>
2022-04-18 17:55:28 +0530

    first commit

---
+++ src/main/java/base/BaseTest.java
@@ -0,0 +1,23 @@
+package base;
+
+
+import factory.DriverManager;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import  org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void startDriver(){
+      driver = new DriverManager().initializeDriver();
+    }
+
+    @AfterMethod
+
+    public void quitDriver(){
+        driver.quit();
+        }
+
+}
---
+++ src/test/java/HomePageTest.java
@@ -0,0 +1,14 @@
+import base.BaseTest;
+import org.testng.annotations.Test;
+
+public class HomePageTest extends BaseTest {
+
+    @Test
+    public void testHomePageTitle()
+    {
+        driver.get("https:https://askomdch.com");
+
+
+
+    }
+}
